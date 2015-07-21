b1944df101c0cafb9472ec61ec1a6858c82ed1b2
Señor Sisk <jsisk@groupon.com>
2015-07-21 13:49:20 -0500

    Appease rubocop

--- config/initializers/010_dotenv.rb
+++ config/initializers/010_dotenv.rb
@@ -1,5 +1,3 @@
-# if GG.local_environment?
-  # Dotenv will not overwrite values by default, so load the local overrides first.
-  Dotenv.load(".env.#{GG.env}.local")
-  Dotenv.load(".env.#{GG.env}")
-# end
+# Dotenv will not overwrite values by default, so load the local overrides first.
+Dotenv.load(".env.#{GG.env}.local")
+Dotenv.load(".env.#{GG.env}")
